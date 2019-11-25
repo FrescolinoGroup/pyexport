f9930dc271ef1ca8d7649becabeeaf65e2cdc1c9
Dominik Gresch <a-dogres@microsoft.com>
2019-11-25 22:45:15 +0100

    Install from PyPI with --pre flag (enable 'dev').

--- .ci/install_script.sh
+++ .ci/install_script.sh
@@ -11,7 +11,7 @@ cd ${TRAVIS_BUILD_DIR}
 
 case "$INSTALL_TYPE" in
     test_pypi)
-        pip install fsc.export[test]
+        pip install --pre fsc.export[test]
         ;;
     test_sdist)
         python setup.py sdist
